7ab7069e5dc75dec4ab5de5c24dd054e0a6bf658
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-02 14:53:34 +0900

    feat: 메뉴 이름을 입력받고 확인 버튼을 누르면 메뉴를 추가한다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,29 @@
 // Step1: 돔 조작과 이벤트 핸들링으로 메뉴 관리하기
 // TODO 1. 메뉴 추가
-// - [ ] 메뉴 이름을 입력받고 확인 버튼을 누르면 메뉴를 추가된다.
+// - [v] 메뉴 이름을 입력받고 확인 버튼을 누르면 메뉴를 추가된다.
 // - [ ] 메뉴 이름을 입력받고 엔터키 입력으로 메뉴를 추가한다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 // - [ ] 추가되는 메뉴의 아래 마크업은 < ul id = "espresso-menu-list" class="mt-3 pl-0" ></ > 안에 삽입해야 한다.
 
-console.log("hi")
+const input = document.querySelector("#espresso-menu-name")
+const button = document.querySelector("#espresso-menu-submit-button")
+const list = document.querySelector("#espresso-menu-list")
+
+function addMenu(name) {
+  const menuItem = document.createElement("li")
+  menuItem.class = "menu-list-item d-flex items-center py-2"
+  menuItem.innerHTML = name
+  list.appendChild(menuItem)
+}
 
 function App() {
-  document.querySelector("#")
+  button.addEventListener("click", () => {
+    addMenu(input.value)
+  })
 }
+
+App()
 
 // TODO 2. 메뉴 수정
 // - [ ] 메뉴의 수정 버튼 클릭 이벤트가 발생하면, 브라우저 prompt 인터페이스가 뜬다.
