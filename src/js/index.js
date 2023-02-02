441cec5cd5a25dd714dd27d81db7d352e5993149
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-02 15:21:42 +0900

    feat: 메뉴 추가시 수정/삭제 버튼 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 // Step1: 돔 조작과 이벤트 핸들링으로 메뉴 관리하기
+
+const form = document.querySelector("#espresso-menu-form")
+const input = document.querySelector("#espresso-menu-name")
+const button = document.querySelector("#espresso-menu-submit-button")
+const list = document.querySelector("#espresso-menu-list")
+
 // TODO 1. 메뉴 추가
 // - [v] 메뉴 이름을 입력받고 확인 버튼을 누르면 메뉴를 추가한다.
 // - [v] 메뉴 이름을 입력받고 엔터키 입력으로 메뉴를 추가한다.
@@ -6,24 +12,44 @@
 // - [v] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 // - [v] 추가되는 메뉴의 아래 마크업은 <ul id ="espresso-menu-list" class="mt-3 pl-0" ></ul> 안에 삽입해야 한다.
 
-const form = document.querySelector("#espresso-menu-form")
-const input = document.querySelector("#espresso-menu-name")
-const button = document.querySelector("#espresso-menu-submit-button")
-const list = document.querySelector("#espresso-menu-list")
+function createElem(item, name) {
+  const span = document.createElement("span")
+  span.className = "w-100 pl-2 menu-name"
+  span.innerHTML = name
+
+  const edit = document.createElement("button")
+  edit.type = "button"
+  edit.className = "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  edit.innerHTML = "수정"
+
+  const remove = document.createElement("button")
+  remove.type = "button"
+  remove.className = "bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  remove.innerHTML = "삭제"
+
+  item.appendChild(span)
+  item.appendChild(edit)
+  item.appendChild(remove)
+}
 
 function addMenu() {
-  const menuName = input.value
-  if (!menuName) return
+  const name = input.value
+  if (!name) return
 
-  const menuItem = document.createElement("li")
-  menuItem.className = "menu-list-item d-flex items-center py-2"
+  const item = document.createElement("li")
+  item.className = "menu-list-item d-flex items-center py-2"
 
-  menuItem.innerHTML = menuName
+  createElem(item, name)
 
-  list.appendChild(menuItem)
+  list.appendChild(item)
 
   input.value = ""
 }
+
+// TODO 2. 메뉴 수정
+// - [ ] 메뉴의 수정 버튼 클릭 이벤트가 발생하면, 브라우저 prompt 인터페이스가 뜬다.
+// - [ ] 메뉴 수정 시 prompt 인터페이스에 수정 메뉴명을 기입하고 확인 버튼을 누르면 메뉴 이름이 업데이트된다.
+function updateMenu() {}
 
 function App() {
   form.addEventListener("submit", (e) => {
@@ -41,9 +67,6 @@ function App() {
 
 App()
 
-// TODO 2. 메뉴 수정
-// - [ ] 메뉴의 수정 버튼 클릭 이벤트가 발생하면, 브라우저 prompt 인터페이스가 뜬다.
-// - [ ] 메뉴 수정 시 prompt 인터페이스에 수정 메뉴명을 기입하고 확인 버튼을 누르면 메뉴 이름이 업데이트된다.
 // TODO 3. 메뉴 삭제
 // - [ ] 메뉴의 삭제 버튼을 누르면 브라우저 confirm 인터페이스가 뜬다.
 // - [ ] 메뉴 삭제 시 confirm 인터페이스에 확인 버튼을 누르면 메뉴가 삭제된다.
