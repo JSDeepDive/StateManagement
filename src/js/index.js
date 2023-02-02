a52bb1728cedc5567d2b1909ecc1e2248f963043
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-02 15:48:29 +0900

    feat: 메뉴 삭제

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const form = document.querySelector("#espresso-menu-form")
 const input = document.querySelector("#espresso-menu-name")
 const button = document.querySelector("#espresso-menu-submit-button")
 const list = document.querySelector("#espresso-menu-list")
+let nums = 0
 
 // TODO 1. 메뉴 추가
 // - [v] 메뉴 이름을 입력받고 확인 버튼을 누르면 메뉴를 추가한다.
@@ -13,6 +14,8 @@ const list = document.querySelector("#espresso-menu-list")
 // - [v] 추가되는 메뉴의 아래 마크업은 <ul id ="espresso-menu-list" class="mt-3 pl-0" ></ul> 안에 삽입해야 한다.
 
 function createElem(item, name) {
+  nums += 1
+
   const span = document.createElement("span")
   span.className = "w-100 pl-2 menu-name"
   span.innerHTML = name
@@ -29,6 +32,9 @@ function createElem(item, name) {
   remove.type = "button"
   remove.className = "bg-gray-50 text-gray-500 text-sm menu-remove-button"
   remove.innerHTML = "삭제"
+  remove.addEventListener("click", () => {
+    removeMenu(item)
+  })
 
   item.appendChild(span)
   item.appendChild(edit)
@@ -53,9 +59,18 @@ function addMenu() {
 // - [v] 메뉴의 수정 버튼 클릭 이벤트가 발생하면, 브라우저 prompt 인터페이스가 뜬다.
 // - [v] 메뉴 수정 시 prompt 인터페이스에 수정 메뉴명을 기입하고 확인 버튼을 누르면 메뉴 이름이 업데이트된다.
 function updateMenu(span) {
-  const name = prompt("메뉴명을 수정하세요", span.innerHTML)
-  if (name === null) return
-  span.innerHTML = name
+  const ret = prompt("메뉴명을 수정하세요", span.innerHTML)
+  if (ret === null) return
+  span.innerHTML = ret
+}
+
+// TODO 3. 메뉴 삭제
+// - [v] 메뉴의 삭제 버튼을 누르면 브라우저 confirm 인터페이스가 뜬다.
+// - [v] 메뉴 삭제 시 confirm 인터페이스에 확인 버튼을 누르면 메뉴가 삭제된다.
+function removeMenu(item) {
+  const ret = confirm("정말 삭제하시겠습니까?")
+  if (!ret) return
+  list.removeChild(item)
 }
 
 function App() {
@@ -74,8 +89,5 @@ function App() {
 
 App()
 
-// TODO 3. 메뉴 삭제
-// - [ ] 메뉴의 삭제 버튼을 누르면 브라우저 confirm 인터페이스가 뜬다.
-// - [ ] 메뉴 삭제 시 confirm 인터페이스에 확인 버튼을 누르면 메뉴가 삭제된다.
 // TODO 4. 기타
 // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
