6092225610aac5390ae125fb1babe6e9d67c2b7d
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-03 16:55:30 +0900

    feat:createElem 함수를 data-* 속성을 적용한 html 템플릿으로 대체

--- src/js/index.js
+++ src/js/index.js
@@ -10,35 +10,25 @@ let nums = 0
 // TODO 1. 메뉴 추가
 // - [v] form 요소의 submit 이벤트로 addMenu 이벤트 핸들러 바인딩
 
-function createElem(item, name) {
-  const span = document.createElement("span")
-  span.className = "w-100 pl-2 menu-name"
-  span.innerHTML = name
-
-  const edit = document.createElement("button")
-  edit.type = "button"
-  edit.className = "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-  edit.innerHTML = "수정"
-  edit.addEventListener("click", () => {
-    updateMenu(span)
-  })
-
-  const remove = document.createElement("button")
-  remove.type = "button"
-  remove.className = "bg-gray-50 text-gray-500 text-sm menu-remove-button"
-  remove.innerHTML = "삭제"
-  remove.addEventListener("click", () => {
-    removeMenu(item)
-  })
-
-  item.appendChild(span)
-  item.appendChild(edit)
-  item.appendChild(remove)
-}
-
 function updateTotal() {
   cnt.innerHTML = `총 ${nums}개`
 }
+
+const createHTML = (name) => `
+				<span class="w-100 pl-2 menu-name">${name}</span>
+				<button
+					type="button"
+					class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button data-edit"
+				>
+					수정
+				</button>
+				<button
+					type="button"
+					class="bg-gray-50 text-gray-500 text-sm menu-remove-button data-remove"
+				>
+					삭제
+				</button>
+			`
 
 function addMenu() {
   const name = input.value
@@ -46,8 +36,7 @@ function addMenu() {
 
   const item = document.createElement("li")
   item.className = "menu-list-item d-flex items-center py-2"
-
-  createElem(item, name)
+  item.innerHTML = createHTML(name)
 
   list.appendChild(item)
 
@@ -58,7 +47,7 @@ function addMenu() {
 }
 
 // TODO 2. 메뉴 수정/삭제
-// - [ ] createElem 함수를 data-* 속성을 적용한 html 템플릿으로 대체.
+// - [v] createElem 함수를 data-* 속성을 적용한 html 템플릿으로 대체.
 // - [ ] 메뉴 수정과 삭제 이벤트 처리를 <ul> 요소에 위임.
 function updateMenu(span) {
   const ret = prompt("메뉴명을 수정하세요", span.innerHTML)
