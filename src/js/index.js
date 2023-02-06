d0cc50d1584cde4e5553f69b827d8e652e535336
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-06 17:00:22 +0900

    chore: v3 DOM 접근 과정 추상화 함수 setState, render 구현

--- src/js/index.js
+++ src/js/index.js
@@ -21,8 +21,10 @@ const createItem = (name, key) => `
 			`
 
 const $ = (selector) => document.querySelector(selector)
-const list = $("#espresso-menu-list")
 
+/*
+ * state: 컴포넌트 내부에서 관리할 상태값
+ **/
 let state = {
   menu: [
     "long black",
@@ -34,8 +36,49 @@ let state = {
   ],
 }
 
-let renderTimes = 0
+/*
+ * render: 맨 처음이나 컴포넌트 상태 변화시 DOM 요소 조정 과정을 추상화한 함수
+ **/
+const render = () => {
+  const { menu } = state
 
+  const form = $("form")
+  const list = $("ul")
+
+  list.innerHTML = `
+		<ul id="espresso-menu-list" class="mt-3 pl-0">
+			${menu.map((name, idx) => createItem(name, idx)).join("")}
+		</ul>
+	`
+  // 메뉴 추가
+  form.addEventListener("submit", addEventHandler)
+
+  // 메뉴 수정/삭제
+  list.addEventListener("click", updateRemoveEventHandler)
+
+  updateTotal()
+}
+
+/*
+ * setState: 컴포넌트 내부 상태 업데이트 하는 과정을 추상화한 함수
+ **/
+const setState = (newState) => {
+  const prevState = state
+  state = { ...state, ...newState }
+  console.log(
+    `[setState]: \n(prevState) ${JSON.stringify(
+      prevState
+    )} \n(currState) ${JSON.stringify(state)}`
+  )
+  render()
+}
+
+render()
+
+/*
+ * 하단의 addMenu, updateMenu, removeMenu, updateTotal은 render 함수 내부에서 호출하는 함수
+ * DOM 요소에는 값을 가져올 때만 접근하고, 값이 변경될 때 DOM 요소를 직접 접근하지 않고 setState로 처리함
+ **/
 const addMenu = () => {
   const { menu } = state
   const input = $("input")
@@ -88,6 +131,11 @@ function updateTotal() {
   cnt.innerText = `총 ${menu.length}개`
 }
 
+/*
+ * 동일 이벤트에 대해 동일한 이벤트를 여러 번 등록하지 않도록 하기 위해, 별도의 함수로 분리.
+ * addEventListener는 이벤트 핸들러를 큐에 넣기 전, 참조 메모리 주소에 의해 동일성 여부 검사함.
+ **/
+
 function addEventHandler(e) {
   e.preventDefault()
   addMenu()
@@ -101,39 +149,3 @@ function updateRemoveEventHandler(e) {
     removeMenu(e)
   }
 }
-
-const render = () => {
-  console.log("[Render] Called!")
-  renderTimes += 1
-
-  const { menu } = state
-
-  const form = $("form")
-  const list = $("ul")
-
-  list.innerHTML = `
-		<ul id="espresso-menu-list" class="mt-3 pl-0">
-			${menu.map((name, idx) => createItem(name, idx)).join("")}
-		</ul>
-	`
-  // 메뉴 추가
-  form.addEventListener("submit", addEventHandler)
-
-  // 메뉴 수정/삭제
-  list.addEventListener("click", updateRemoveEventHandler)
-
-  updateTotal()
-}
-
-const setState = (newState) => {
-  const prevState = state
-  state = { ...state, ...newState }
-  console.log(
-    `[setState]: \n(prevState) ${JSON.stringify(
-      prevState
-    )} \n(currState) ${JSON.stringify(state)}`
-  )
-  render()
-}
-
-render()
