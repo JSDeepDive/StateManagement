dc27ae24b1e2a7fa615792f4d0d24a9a202db1cd
ericagong <48196721+ericagong@users.noreply.github.com>
2023-02-06 16:25:04 +0900

    feat: setState 통한 메뉴 수정 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,19 @@
 // Step1: 돔 조작과 이벤트 핸들링으로 메뉴 관리하기(v3)
 
-const createItem = (name) => `
+const createItem = (name, key) => `
 			<li class="menu-list-item d-flex items-center py-2">
 				<span class="w-100 pl-2 menu-name">${name}</span>
 				<button
 					type="button"
 					class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button data-edit"
+					data-index=${key}
 				>
 					수정
 				</button>
 				<button
 					type="button"
 					class="bg-gray-50 text-gray-500 text-sm menu-remove-button data-remove"
+					data-index=${key}
 				>
 					삭제
 				</button>
@@ -32,6 +34,8 @@ let state = {
   ],
 }
 
+let renderTimes = 0
+
 const addMenu = () => {
   const { menu } = state
   const input = $("input")
@@ -40,22 +44,64 @@ const addMenu = () => {
 
   if (!name) return
 
-  setState({ menu: [...menu, name] })
+  setState({ menu: [...menu, name] }) // setState 통해 메뉴 추가시 자동으로 DOM 요소 처리함
 
   input.value = ""
 }
 
-const render = () => {
+function updateMenu(e) {
   const { menu } = state
+
+  const prevName = e.target.closest("li").querySelector("span").innerText
+  const index = Number(e.target.dataset.index)
+
+  const newName = prompt("메뉴명을 수정하세요", prevName)
+
+  if (newName === null) return
+
+  setState({
+    menu: menu.map((name, idx) => {
+      if (index === idx) return newName
+      return name
+    }),
+  })
+}
+
+function removeMenu(e) {
+  const li = e.target.closest("li")
+  const ret = confirm("정말 삭제하시겠습니까?")
+  if (!ret) return
+  li.remove()
+  updateTotal()
+}
+
+const render = () => {
+  renderTimes += 1
+
+  const { menu } = state
+
+  const form = $("form")
+  const list = $("ul")
+
   list.innerHTML = `
 		<ul id="espresso-menu-list" class="mt-3 pl-0">
-			${menu.map((name) => createItem(name)).join("")}
+			${menu.map((name, idx) => createItem(name, idx)).join("")}
 		</ul>
 	`
   // 메뉴 추가
-  $("form").addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault()
     addMenu()
+  })
+
+  // 메뉴 수정/삭제
+  list.addEventListener("click", (e) => {
+    if (e.target.classList.contains("data-edit")) {
+      updateMenu(e)
+    }
+    if (e.target.classList.contains("data-remove")) {
+      removeMenu(e)
+    }
   })
 }
 
@@ -72,28 +118,11 @@ const setState = (newState) => {
 
 render()
 
-// const form = $("#espresso-menu-form")
-// const input = $("#espresso-menu-name")
 // const cnt = $(".menu-count")
 
 // function updateTotal() {
 //   const num = list.querySelectorAll("li").length
 //   cnt.innerText = `총 ${num}개`
-// }
-
-// function updateMenu(e) {
-//   const span = e.target.closest("li").querySelector("span")
-//   const ret = prompt("메뉴명을 수정하세요", span.innerHTML)
-//   if (ret === null) return
-//   span.innerHTML = ret
-// }
-
-// function removeMenu(e) {
-//   const li = e.target.closest("li")
-//   const ret = confirm("정말 삭제하시겠습니까?")
-//   if (!ret) return
-//   li.remove()
-//   updateTotal()
 // }
 
 // function App() {
